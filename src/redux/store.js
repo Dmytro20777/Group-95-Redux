a2625a559f4ab803ebc71ba63c12381a83a8b593
Dmytro20777 <08dmutro11@gmail.com>
2024-03-03 11:06:48 +0200

    Task1

---
+++ src/redux/store.js
@@ -0,0 +1,35 @@
+import { configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { todosReducer } from './todosSlice'
+
+
+
+const persistConfig = {
+    key: 'todo',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, todosReducer)
+
+export const store = configureStore({
+  reducer: {todos: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store);
+
---
+++ src/redux/todosSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    items: []
+}
+
+const todosSlice = createSlice({
+  name: "todos",
+   initialState,
+  
+  reducers: {
+      addTodo(state, {payload}) {
+        //   state.items.push(payload);
+          state.items = [...state.items, payload]
+    },
+      deleteTodo(state, {payload}) {
+        state.items = state.items.filter((item) => item.id !== payload)
+    }
+  },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+
+export const todosReducer = todosSlice.reducer;
